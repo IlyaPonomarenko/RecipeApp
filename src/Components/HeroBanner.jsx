b349f6bd07e9587a22dc666248f553175b2340ec
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-12-22 13:04:04 +0200

    added components and stylings

--- src/Components/HeroBanner.jsx
+++ src/Components/HeroBanner.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import "../Styles/HeroBanner.css"
 import { Link } from "react-router-dom";
 
 const HeroBanner = () => {
   return (
-    <div>
-      <video autoPlay="autoplay" muted={true} loop={true} className="video">
-        <source src={require("../Assets/video.mp4")} type="video/mp4" />
-      </video>
-      <div className="banner-header">
+    <div className="Banner-wrapper">
+      <div className="Banner-header">
         <h1>TasteIT</h1>
         <p>TasteIT is a recipe app which is made in REACT22S group lessons.</p>
         <div>
-          <Link className="button" to="allrecipies">
+          <Link className="Banner-button" to="allrecipies">
             Check out all recipies
           </Link>
         </div>
       </div>
+      <video autoPlay="autoplay" muted={true} loop={true} className="Banner-video">
+        <source src={require("../Assets/video.mp4")} type="video/mp4" />
+      </video>
     </div>
   );
 };
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
-import HeroBanner from './HeroBanner';
+import React from "react";
+import HeroBanner from "./HeroBanner";
+import { Link } from "react-router-dom";
 const Home = () => {
-    return (
-        <div>
-            <HeroBanner/>
+  return (
+    <div className="Home-wrapper">
+      <HeroBanner />
+      <div className="Home-header">
+        <h2>Looking for new recipies?</h2>
+      </div>
+      <div className="Home-cards-wrapper">
+        <div className="Home-card">
+          <h2>Browse recipes</h2>
+          <p>
+            Find your favourites in this collection. You can search recipes
+            based on name or country
+          </p>
+          <Link to="allrecipies">All recipes</Link>
         </div>
-    );
+
+        <div className="Home-card">
+          <h2>Add a recipe</h2>
+          <p>
+           Recipe from your country is missing? No worries, add one!
+          </p>
+          <Link to="addnewrecipe">Add recipe</Link>
+        </div>
+
+        <div className="Home-card">
+          <h2>Want to know more about our projects?</h2>
+          <p>
+            Visit our programme homepage
+          </p>
+          <a href="https://en.bc.fi" target="_blanj noreferer"></a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
