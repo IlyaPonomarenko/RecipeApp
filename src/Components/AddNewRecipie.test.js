edc1fd93824123158d11054734688287855b985e
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-23 10:09:33 +0200

    test template

---
+++ src/Components/AddNewRecipie.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import AddNewRecipie from "./AddNewRecipie";
+
+it("getting data from an api", () => {
+    
+      test('renders users when API call succeeds', async () => {})
+    
+      test('renders error when API call fails', async () => {})
+});
