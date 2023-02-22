63c1e97f6fd96160659d3b3e9ae423f3fd467fd4
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-02-22 17:38:14 +0200

    tests

--- src/Components/__test__/Card.test.js
+++ src/Components/__test__/Card.test.js
@@ -4,12 +4,15 @@ import "@testing-library/jest-dom";
 import Card from "../Card";
 import { BrowserRouter } from "react-router-dom";
 
-it("should check if Link component renders inside of Card", () => {
-  render(
-    <BrowserRouter>
-      <Card />
-    </BrowserRouter>
-  );
-  const linkElement = screen.getByRole("link");
-  expect(linkElement).toBeInTheDocument();
-});
+describe("1 - Card - Check if Link component renders inside of Card", ()=>{
+  it("renders Card", () => {
+    render(
+      <BrowserRouter>
+        <Card />
+      </BrowserRouter>
+    );
+    const linkElement = screen.getByRole("link");
+    expect(linkElement).toBeInTheDocument();
+  });
+})
+
